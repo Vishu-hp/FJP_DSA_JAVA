94a7f8c017986edab8031e6c7686c71f0d08d9dc
DOM-Vishu <70435386+Vishu-hp@users.noreply.github.com>
2024-01-23 19:51:31 +0530

    Create CountNumberOfWaysToPartitionASetIntoKSubsets_Recursion.java

---
+++ L2/Recursion/CountNumberOfWaysToPartitionASetIntoKSubsets_Recursion.java
@@ -0,0 +1,25 @@
+// RECURSIVE SOLUTION
+
+import java.io.*;
+ 
+class GFG
+{
+    public static int countP(int n, int k)
+    {
+       // Base cases
+       if (n == 0 || k == 0 || k > n)
+          return 0;
+       if (k == 1 || k == n)
+          return 1;
+ 
+       // S(n+1, k) = k*S(n, k) + S(n, k-1)
+       return (k * countP(n - 1, k) 
+              + countP(n - 1, k - 1));
+    }
+ 
+       public static void main(String args[])
+    {
+       System.out.println(countP(3, 2));
+ 
+    }
+}
