5eaebc1d3e482410490f6626df57416eea629ff9
DOM-Vishu <70435386+Vishu-hp@users.noreply.github.com>
2025-01-01 21:52:05 +0530

    Update lc-787-cheapest-flights-within-k-stops.java

--- LeetcodeContests/biweekly-145/c-learning/lc-787-cheapest-flights-within-k-stops.java
+++ LeetcodeContests/biweekly-145/c-learning/lc-787-cheapest-flights-within-k-stops.java
@@ -1,9 +1,10 @@
 // great problem
 // dijkstra doesn't work because of constraints of stop, answer can also on path with more price as path with less price can have stops constraints
-// dijkstra without visisted check will give tle 
+// dijkstra without visisted check will give tle and using excessive looping check was accepted
 // dfs also gave tle , needs some improvement 
 // bfs was accepted, with a check below which avoids over calculations
 
+// bfs with excessive looping check
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
         Map<Integer, Map<Integer, Integer>> map = new HashMap<>();
@@ -50,5 +51,64 @@ class Solution {
         }
         return min == Integer.MAX_VALUE ? -1 : min;
 
+    }
+}
+
+// dijkstra with excessive looping check
+class Solution {
+    public Map<Integer, Map<Integer, Integer>> createGraph(int[][] flights) {
+        Map<Integer, Map<Integer, Integer>> graph = new HashMap<>();
+
+        for (int[] flight : flights) {
+            int src = flight[0], des = flight[1], prc = flight[2];
+            graph.putIfAbsent(src, new HashMap<>());
+            graph.get(src).put(des, prc);
+        }
+
+        return graph;
+    }
+
+    public int dijkstra(Map<Integer, Map<Integer, Integer>> graph, int src, int dst, int k
+    , int n) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
+        pq.add(new int[] { src, 0, 0 });
+        int[] vis = new int[n];
+        Arrays.fill(vis, 0);
+        vis[src]++;
+        int ans = Integer.MAX_VALUE;
+
+        while (pq.size() > 0) {
+            int[] rem = pq.remove();
+
+            // removes excessive looping
+            if (vis[rem[0]] > n) {
+                continue;
+            }
+
+            vis[rem[0]]++;
+            if (rem[0] == dst) {
+                ans = Math.min(ans, rem[1]);
+            }
+
+            Map<Integer, Integer> inMap = graph.get(rem[0]);
+            if (inMap != null) {
+                for (Map.Entry<Integer, Integer> x : graph.get(rem[0]).entrySet()) {
+                    int nbr = x.getKey(), prc = x.getValue();
+
+                    if (rem[2]+1 < k+1 || (rem[2] + 1 == k + 1 && nbr == dst)) {
+                        pq.add(new int[] { nbr, prc + rem[1], rem[2] + 1 });
+                    }
+                }
+            }
+        }
+
+        return ans == Integer.MAX_VALUE ? -1 : ans;
+    }
+
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        Map<Integer, Map<Integer, Integer>> graph = createGraph(flights);
+        int ans;
+        ans = dijkstra(graph, src, dst, k, n);
+        return ans;
     }
 }
