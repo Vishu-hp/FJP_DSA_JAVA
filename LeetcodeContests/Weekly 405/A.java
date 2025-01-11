6de3bf7e68997b0498e18722db1c7b7b4387eb56
DOM-Vishu <70435386+Vishu-hp@users.noreply.github.com>
2025-01-11 21:37:12 +0530

    Update A.java

--- LeetcodeContests/Weekly 405/A.java
+++ LeetcodeContests/Weekly 405/A.java
@@ -1,1 +1,17 @@
+// decent A , stringbuilder use
 
+class Solution {
+    public String getEncryptedString(String s, int k) {
+        StringBuilder str = new StringBuilder();
+        str.append(s);
+
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            int idx = (i + k) % n;
+
+            str.setCharAt(i, s.charAt(idx));
+        }
+
+        return str.toString();
+    }
+}
